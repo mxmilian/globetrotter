26c2efa8b42934cbd94efe2d24d2bc4a095f171a
maxiu271299 <maksymilian.kierski@gmail.com>
2020-02-05 00:48:07 +0100

    Add my places view

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 import MainNavigation from 'shared/components/Navigation/MainNavigation';
 import Users from 'user/pages/Users';
 import NewPlace from 'places/pages/NewPlace';
+import UserPlaces from 'places/pages/UserPlaces';
 
 const App = () => {
   return (
@@ -12,6 +13,9 @@ const App = () => {
         <Switch>
           <Route path="/" exact>
             <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
           </Route>
           <Route path="/places/new" exact>
             <NewPlace />
