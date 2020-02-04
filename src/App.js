089f2c7a0f7f8484e4372a47febff52957bb0355
maxiu271299 <maksymilian.kierski@gmail.com>
2020-02-04 22:06:17 +0100

    Add absolute path, first component wich is a user and basic styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import Users from 'user/pages/Users';
+import NewPlace from "places/pages/NewPlace";
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
